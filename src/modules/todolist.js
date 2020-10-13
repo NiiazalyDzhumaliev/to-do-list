56ac12a28c753677f341a7919010770e606b357d
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-10-13 21:15:03 +0600

    Fix list populating

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -5,26 +5,14 @@ const toDoListFactory = (title, description, dueDate, prior) => {
 };
 
 function showToDoList() {
-  let title = document.getElementById('tod-title');
-  let desc = document.getElementById('desc');
-  let d-date = document.getElementById('d-date');
-  let prio = document.getElementById('prior');
-
-
-
-  // const toDoList = document.querySelector('.todolist');
-  // const ul = document.createElement('ul');
-  // ul.setAttribute('class', 'todolist-list');
-  // toDoList.appendChild(ul);
-  // const li = document.createElement('li');
-  // li.setAttribute('class', 'todolist-item');
-  // ul.appendChild(li);
-  toDoListArr.forEach((list) => {
-    title.innerHTML = list.title;
-    desc.innerHTML = list.description;
-    d-date.innerHTML = list.dueDate;
-    prio.innerHTML = list.prior;
-  });
+  const toDoList = document.querySelector('.todolist');
+  const ul = document.createElement('ul');
+  ul.setAttribute('class', 'todolist-list');
+  toDoList.appendChild(ul);
+  ul.innerHTML += `<li>${toDoListArr[toDoListArr.length - 1].title}</li>`;
+  ul.innerHTML += `<li>${toDoListArr[toDoListArr.length - 1].description}</li>`;
+  ul.innerHTML += `<li>${toDoListArr[toDoListArr.length - 1].dueDate}</li>`;
+  ul.innerHTML += `<li>${toDoListArr[toDoListArr.length - 1].prior}</li>`;
 }
 
 const createToDoList = () => {
