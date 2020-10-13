bed76dd59850c54b6e8334c85ab716632422d166
paulzay <paulotieno2@gmail.com>
2020-10-13 17:29:05 +0300

    display the todolist

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -5,18 +5,25 @@ const toDoListFactory = (title, description, dueDate, prior) => {
 };
 
 function showToDoList() {
-  const toDoList = document.querySelector('.todolist');
-  const ul = document.createElement('ul');
-  ul.setAttribute('class', 'todolist-list');
-  toDoList.appendChild(ul);
-  const li = document.createElement('li');
-  li.setAttribute('class', 'todolist-item');
-  ul.appendChild(li);
+  let title = document.getElementById('tod-title');
+  let desc = document.getElementById('desc');
+  let d-date = document.getElementById('d-date');
+  let prio = document.getElementById('prior');
+
+
+
+  // const toDoList = document.querySelector('.todolist');
+  // const ul = document.createElement('ul');
+  // ul.setAttribute('class', 'todolist-list');
+  // toDoList.appendChild(ul);
+  // const li = document.createElement('li');
+  // li.setAttribute('class', 'todolist-item');
+  // ul.appendChild(li);
   toDoListArr.forEach((list) => {
-    li.innerHTML = list.title;
-    li.innerHTML = list.description;
-    li.innerHTML = list.dueDate;
-    li.innerHTML = list.prior;
+    title.innerHTML = list.title;
+    desc.innerHTML = list.description;
+    d-date.innerHTML = list.dueDate;
+    prio.innerHTML = list.prior;
   });
 }
 
